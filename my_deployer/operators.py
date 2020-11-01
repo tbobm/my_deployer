dc0709f2ed837fb8f57315e82aaefcfdfdd38223
Theo Massard <theo@memento-solution.com>
2020-11-01 19:21:59 +0100

    Handle existing containers (kill/restore)

--- my_deployer/operators.py
+++ my_deployer/operators.py
@@ -1,7 +1,7 @@
 """Different connectors used to interact with the remote host."""
 import logging
 import time
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List
 from pathlib import Path
 
 import docker
@@ -9,6 +9,7 @@ from docker.models.containers import Container
 import paramiko
 
 from my_deployer.errors import MyDeployerError
+from my_deployer.utils import should_deploy_based_on_version
 from my_deployer.logs import build_logger
 from my_deployer.remote_commands import INSTALL_DOCKER, ADD_USER_TO_DOCKER_GROUP
 from my_deployer.structs import SSHInfos
@@ -89,6 +90,10 @@ class DockerOperator:
     DEFAULT_LABELS = {
         'management.tool': 'my_deployer',
     }
+    CONTAINER_LABELS = {
+        'image': 'my_deployer.app.image',
+        'tag': 'my_deployer.app.tag',
+    }
 
     def __init__(self, url: str, logger: Optional[logging.Logger] = None):
         self.client = docker.DockerClient(url)
@@ -116,22 +121,109 @@ class DockerOperator:
         self.logger.info('successfully built service %s:%s (%s)', name, tag, image.id)
         self.logger.info('image=%s size=%d bytes', image.short_id, image.attrs.get('Size'))
 
+    def handle_running_containers(self, image_name: str, image_tag: str) -> List[Container]:
+        """Look for and stop the container running using the fully qualified_image."""
+        target_labels = {
+            'label': [
+                f"{self.CONTAINER_LABELS['image']}={image_name}",
+            ]
+        }
+        containers = self.client.containers.list(
+            filters=target_labels,
+        )
+        self.logger.info('looking for existing containers using image=%s', image_name)
+        if len(containers) == 0:
+            self.logger.info('no previous container')
+            return []
+
+        self.logger.info(
+            'checking wether or not we should deploy image=%s:%s',
+            image_name,
+            image_tag,
+        )
+        older_containers = [
+            cont for cont in containers
+            if should_deploy_based_on_version(cont.image.tags[0], image_tag)
+        ]
+        self.logger.info('found %d containers', len(older_containers))
+        for container in older_containers:
+            self.logger.info(
+                'stopping %s (image=%s)',
+                container.name,
+                container.image.tags[0],
+            )
+            container.stop()
+        return containers
+
+    def _remove_containers(self, containers: List[Container]):
+        """Attempt to remove the previous containers."""
+        self.logger.info('deleting %d previous containers', len(containers))
+        for container in containers:
+            container.remove()
+            self.logger.debug('deleted container %s', container.id)
+        self.logger.info('deleted previous containers')
+
+    def _restore_containers(self, containers: List[Container]):
+        """Attempt to restore previous containers."""
+        self.logger.info('restoring %d previous containers', len(containers))
+        for container in containers:
+            container.start()
+            self.logger.debug('restored container %s', container.id)
+        self.logger.info('restored previous containers')
+
+    def handle_old_containers(self, containers: List[Container], success: bool):
+        """Delete or restart previous containers based on success if any."""
+        if len(containers) == 0:
+            self.logger.info('no previous containers to handle')
+            return
+        if success:
+            self._remove_containers(containers)
+        else:
+            self._restore_containers(containers)
+
     def run_container(self,
                       image_name: str,
                       image_tag: str = 'latest',
                       container_name: Optional[str] = None) -> Container:
-        """Run the container on the remote host."""
-        # TODO: find if container matching name:tag exists
+        """Run the container on the remote host.
+
+        - Look for existing containers (looking for similar `image_name`)
+        - Try to start the target container
+        - Restore or Delete the previous containers if any
+
+        If anything goes wrong while trying to run the target container
+        my_deployer tries to restore the previous containers.
+        If the deployment goes as expected we delete the old containers.
+        """
+        # Look for existing containers
+        containers = self.handle_running_containers(image_name, image_tag)
+        target_image = f"{image_name}:{image_tag}"
+        container_labels = {
+            **self.DEFAULT_LABELS,
+            self.CONTAINER_LABELS['image']: image_name,
+            self.CONTAINER_LABELS['tag']: image_tag,
+        }
+        # Deploy the target container
         self.logger.info('deploying %s:%s', image_name, image_tag)
-        container = self.client.containers.run(
-            f"{image_name}:{image_tag}",
-            detach=True,
-            name=container_name,
-            labels=self.DEFAULT_LABELS,
-        )  # type: Container
-        self.logger.info(
-            'started container id=%s name=%s',
-            container.id,
-            container.name,
-        )
+        try:
+            container = self.client.containers.run(
+                target_image,
+                detach=True,
+                name=container_name,
+                labels=container_labels,
+            )  # type: Container
+            self.logger.info(
+                'started container id=%s name=%s',
+                container.id,
+                container.name,
+            )
+
+            # TODO: if healtcheck, wait for "healthy" or set success as False
+            success = True
+        except docker.errors.DockerException:
+            self.logger.error('failed to start container')
+            success = False
+
+        self.handle_old_containers(containers, success)
+
         return container
